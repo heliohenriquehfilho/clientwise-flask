a6506fb54dc24969da26b57974a46c842240c441
heliohenriquehfilho@gmail.com <heliohenriquehfilho@gmail.com>
2025-01-11 14:50:28 -0300

    update para render

--- main.py
+++ main.py
@@ -322,5 +322,6 @@ def cadastro_venda():
     return render_template("sales.html", clientes=clientes, produtos=produtos, vendedores=vendedores)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
